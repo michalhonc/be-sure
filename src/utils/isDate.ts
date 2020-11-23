87e84efbab430b00c106a79fe836f409e0253e50
michalhonc <honc355@gmail.com>
2020-11-23 15:24:38 +0100

    Update isDate.ts

---
+++ src/utils/isDate.ts
@@ -0,0 +1,1 @@
+// find a way to determine if a given value (string, number, object) is a date or can be converted to date. This is less strict than isDateObject which counts only with JS Date object.
